49d5e7632188312899125854ccae9a71a0878c8a
RodrigoRM <ra.ramirez1993@gmail.com>
2020-06-19 17:01:56 -0400

    act

--- COVID19/vistas/predicciones.py
+++ COVID19/vistas/predicciones.py
@@ -35,7 +35,7 @@ grupo_fallecidos = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Dat
 fallecidos_por_region = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto14/FallecidosCumulativo.csv')
 
 
-seasonal_periods_casos = 7
+seasonal_periods_casos = 2
 
 
 
